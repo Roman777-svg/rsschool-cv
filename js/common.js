eccfb78bd78a5136ac09407e6e07fafa3690a58a
Roman777-svg <rolros7@gmail.com>
2022-08-06 23:25:25 +0300

    feat: add slider Js on slider html

--- js/common.js
+++ js/common.js
@@ -7,6 +7,8 @@ const btnLogin = document.querySelector('.btn-login');
 const popup_container = document.querySelector('.popup_container');
 
 
+
+
 //POPUP
 
 const handleClick = (event) =>{
@@ -29,12 +31,42 @@ popup_container.addEventListener('click',(event) =>{
 
 
  //SLIDER
- const btnactiveClick = document.querySelector('.active-dots');
 
- btnactiveClick.addEventListener('click',(eventclick)=>{
-    if(eventclick.target.className ==='active-dots'){
-        
-    }
- })
+// Кнопка слайдера 
+ const activeDots = document.querySelector('.active-dots');
+// 
+
+
+// Контейнер родителя слайдер 
+ let parent = document.querySelector('#parent_container');
+// 
+
+
+
+  
+ const parentDots = document.querySelector('#parentDots');
+
+ const parentDotsLast = parentDots.lastElementChild
+ const parentDotsFirst = parentDots.firstElementChild
+
+ parentDotsFirst.addEventListener('click',()=>{
+    let elemsFirst = parent.firstElementChild
+    parent.append(elemsFirst); 
+});
+
+ parentDotsLast.addEventListener('click',()=>{
+    let elemsLast = parent.lastElementChild
+
+   parent.prepend(elemsLast); 
+});
 
  //SLIDER
+
+
+
+
+
+ 
+// for (let i = 0; i < elems.children.length; i++) {
+//     console.log(elems.children[i]);
+//   }
